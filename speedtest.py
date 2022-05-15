6094028030f86bfa09444f2bb0f86d243426a549
Stanislaw <fliyd206@gmail.com>
2022-05-15 22:18:16 +0600

    фикс speedtest.py под новую версию юзербота

--- speedtest.py
+++ speedtest.py
@@ -14,6 +14,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# required: speedtest
+
 import speedtest
 
 from pyrogram import Client, types
